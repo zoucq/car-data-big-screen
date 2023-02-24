611bc0c13b3640921f89230eb2992d237a259c55
zcq <zcq@91xiangju.com>
2023-02-24 11:49:46 +0800

    dist3

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # car-data-big-screen
 车联网大屏可视化数据展示 
+vue + echarts
--- src/App.vue
+++ src/App.vue
@@ -47,6 +47,7 @@ export default {
   .scale_box{
     /*background: url("https://mpi.yunjiahealth.com:9443/testdapingweb/img/i-bg.60eca4b0.png") repeat-y center;*/
     /*background: url("https://sugar.aipage.com/static/img-templet/background-2.png") repeat-y center;*/
+
     background: url('~assets/img/map_bg.png') repeat-y center;
     /*background-size: 100% 100%;*/
     opacity: 0;
